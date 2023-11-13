57a0fd406fc4a4224aadf3c6b8b5ed59ba902e1e
SebassNoob <sebastian.ong@hotmail.com>
2023-11-13 15:09:35 +0800

    feat: add attendance, admin columns to ServiceSessionUser

--- interapp-backend/db/entities/service_session_user.ts
+++ interapp-backend/db/entities/service_session_user.ts
@@ -2,6 +2,12 @@ import { Entity, PrimaryColumn, Column, ManyToOne, type Relation } from 'typeorm
 import { User } from './user';
 import { ServiceSession } from './service_session';
 
+export enum AttendanceStatus {
+  Attended = 'Attended',
+  Absent = 'Absent',
+  ValidReason = 'Valid Reason',
+}
+
 @Entity()
 export class ServiceSessionUser {
   @PrimaryColumn()
@@ -12,6 +18,15 @@ export class ServiceSessionUser {
 
   @Column()
   ad_hoc: boolean;
+
+  @Column({
+    type: 'enum',
+    enum: AttendanceStatus,
+  })
+  attended: AttendanceStatus;
+
+  @Column()
+  is_admin: boolean;
 
   @ManyToOne(() => ServiceSession, (service_session) => service_session.service_session_users)
   service_session: Relation<ServiceSession>;
