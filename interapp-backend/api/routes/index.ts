d21334f499beaf55ba9168599230ce8547a17d35
SebassNoob <sebastian.ong@hotmail.com>
2023-11-15 11:33:18 +0800

    feat: add error handling middleware

--- interapp-backend/api/routes/index.ts
+++ interapp-backend/api/routes/index.ts
@@ -1,9 +1,24 @@
 import express from 'express';
 import helloRouter from './hello';
+import 'express-async-errors';
+import { Request, Response, NextFunction } from 'express';
+import { HTTPError } from '@utils/errors';
 
 const app = express();
 const PORT = Number(process.env.API_PORT);
 
 app.use('/api/hello', helloRouter);
+
+app.use((err: HTTPError, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status);
+  res.header(err.headers);
+  res.json({
+    name: err.name,
+    message: err.message,
+    data: err.data,
+  });
+
+  next(err);
+});
 
 app.listen(PORT, () => console.log('Server running on port 8000!'));
