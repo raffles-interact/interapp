a434a1d28f24b38bff793030cbd116f2173803a3
SebassNoob <sebastian.ong@hotmail.com>
2023-11-17 22:04:37 +0800

    feat: impl redis, email service

---
+++ interapp-backend/api/email_handler/index.ts
@@ -0,0 +1,39 @@
+import { createTransport } from 'nodemailer';
+import hbs from 'nodemailer-express-handlebars';
+
+const transporter = createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER as string,
+    pass: process.env.EMAIL_PASSWORD as string,
+  },
+});
+
+const handlebarsOptions: hbs.NodemailerExpressHandlebarsOptions = {
+  viewEngine: {
+    partialsDir: process.cwd() + '/api/email_handler/templates',
+    defaultLayout: false,
+  },
+  viewPath: process.cwd() + '/api/email_handler/templates',
+  extName: '.html',
+};
+
+transporter.use('compile', hbs(handlebarsOptions));
+
+export default transporter;
+
+/*
+await transporter.sendMail({
+  from: {
+    name: 'hi',
+    address: process.env.EMAIL_USER as string,
+  }, // sender address
+  to: ['sebastian.ong@hotmail.com'], // list of receivers
+  subject: "Hello ✔", // Subject line
+  text: "Hello world?", // plain text body
+  html: "<b>Hello world?</b>", // html body
+});
+*/
