ab75ce8fc9d2e42941c4924a27f80c68fb68ac0b
SebassNoob <sebastian.ong@hotmail.com>
2023-12-14 15:48:03 +0800

    feat: add under construction signage

---
+++ interapp-frontend/src/components/UnderConstruction/UnderContruction.tsx
@@ -0,0 +1,11 @@
+import { Text, Title } from '@mantine/core';
+import './styles.css';
+
+export default function UnderConstruction() {
+  return (
+    <div className='under-construction-container'>
+      <Title>🚧 Under Construction</Title>
+      <Text>Stay tuned! This feature has not been implemented yet.</Text>
+    </div>
+  );
+}
