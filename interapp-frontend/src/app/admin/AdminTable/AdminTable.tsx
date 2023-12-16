52b476f3fc4f32eb1909bce4e7e44603809d0222
SebassNoob <sebastian.ong@hotmail.com>
2023-12-16 19:10:23 +0800

    feat: admin page

--- interapp-frontend/src/app/admin/AdminTable/AdminTable.tsx
+++ interapp-frontend/src/app/admin/AdminTable/AdminTable.tsx
@@ -1,20 +1,113 @@
 'use client';
-import { Table } from "@mantine/core"
-import APIClient from "@/api/api_client";
-import { useState } from "react";
-import {User} from "@/providers/AuthProvider/types";
+import { Table, Loader, Pill, TextInput } from '@mantine/core';
+import APIClient from '@/api/api_client';
+import { memo, useEffect, useState } from 'react';
+import { User } from '@/providers/AuthProvider/types';
+import { Permissions } from '@/app/route_permissions';
+import { permissionsMap } from './PermissionsInput/PermissionsInput';
+import EditAction from './EditAction/EditAction';
+import DeleteAction from './DeleteAction/DeleteAction';
+import PageController, { paginateItems } from '@/components/PageController/PageController';
+import {IconSearch} from '@tabler/icons-react';
+import './styles.css';
 
+const fetchUserData = async () => {
+  const apiClient = new APIClient().instance;
+  try {
+    const users: Omit<User, 'permissions'>[] = (await apiClient.get('/user')).data;
+    const perms: { [username: string]: Permissions[] } = (await apiClient.get('/user/permissions'))
+      .data;
+    const usersWithPerms = users.map((user) => ({ ...user, permissions: perms[user.username] }));
+    return usersWithPerms;
+  } catch (e) {
+    throw e;
+  }
+};
 
+const computeSearchItems = (search: string, users: User[]) => {
+  return users.filter((user) => user.username.toLowerCase().includes(search.toLowerCase()) || user.email.toLocaleLowerCase().includes(search.toLowerCase()));
+}
 
 const AdminTable = () => {
-  const apiClient = new APIClient().instance;
-  const [userData, setUserData] = useState<User[]>([])
+  const [userData, setUserData] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
 
-  const fetchUserData = () => {}
+  const usersPerPage = 10;
+
+  const refreshData = () => {
+    setLoading(true);
+    fetchUserData()
+      .then((users) => setUserData(users))
+      .catch((e) => console.error(e));
+    setLoading(false);
+  };
+
+  useEffect(refreshData, []);
 
   return (
     <div>
-      <h1>Admin Table</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <TextInput label='Search for username or email...' value={search} onChange={(e) => setSearch(e.currentTarget.value)} rightSection={<IconSearch/>}/>
+          <Table.ScrollContainer minWidth={1000}>
+            <Table stickyHeader>
+              <Table.Thead>
+                <Table.Tr>
+                  <Table.Th>User ID</Table.Th>
+                  <Table.Th>Username</Table.Th>
+                  <Table.Th>Email</Table.Th>
+                  <Table.Th>Verified</Table.Th>
+                  <Table.Th>Permissions</Table.Th>
+                  <Table.Th>Service Hours</Table.Th>
+                  <Table.Th>Actions</Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>
+                {paginateItems(computeSearchItems(search, userData), page, usersPerPage).map((user) => (
+                  <Table.Tr key={user.username}>
+                    <Table.Td>{user.user_id}</Table.Td>
+                    <Table.Td>{user.username}</Table.Td>
+                    <Table.Td>{user.email}</Table.Td>
+                    <Table.Td>
+                      {user.verified ? (
+                        <span className='admin-table-verified'>Verified</span>
+                      ) : (
+                        <span className='admin-table-unverified'>Unverified</span>
+                      )}
+                    </Table.Td>
+                    <Table.Td>
+                      <div className='admin-table-permissions'>
+                        {user.permissions.map((perm) => (
+                          <Pill key={perm} className='admin-table-permission'>
+                            {permissionsMap[perm]}
+                          </Pill>
+                        ))}
+                      </div>
+                    </Table.Td>
+                    <Table.Td>{user.service_hours}</Table.Td>
+                    <Table.Td className='admin-table-actions'>
+                      <EditAction user={user} refreshData={refreshData} />
+                      <DeleteAction username={user.username} refreshData={refreshData} />
+                    </Table.Td>
+                  </Table.Tr>
+                ))}
+              </Table.Tbody>
+            </Table>
+          </Table.ScrollContainer>
+          <PageController
+            activePage={page}
+            totalPages={Math.ceil(userData.length / usersPerPage)}
+            handlePageChange={(e) => setPage(e)}
+            className='admin-table-pagination'
+          />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
+
+export default memo(AdminTable);
---
+++ interapp-frontend/src/app/admin/AdminTable/PermissionsInput/PermissionsInput.tsx
@@ -0,0 +1,99 @@
+'use client';
+import { PillsInput, Pill, Combobox, CheckIcon, Group, useCombobox, Text } from '@mantine/core';
+import { Permissions } from '@/app/route_permissions';
+import { useEffect, useState } from 'react';
+
+export const permissionsMap: Record<Permissions, string> = {
+  [Permissions.VISTOR]: '🛫 Visitor',
+  [Permissions.CLUB_MEMBER]: '🏬 Member',
+  [Permissions.SERVICE_IC]: '💼 Service IC',
+  [Permissions.MENTORSHIP_IC]: '💼 Mentorship IC',
+  [Permissions.EXCO]: '🛠️ Exco',
+  [Permissions.ATTENDANCE_MANAGER]: '🛠️ Attendance Manager',
+  [Permissions.ADMIN]: '👑 Admin',
+};
+
+const getKeyByValue = (object: Record<string, string>, value: string) =>
+  Object.keys(object).find((key) => object[key] === value);
+
+const PermissionsInput = ({
+  defaultValues,
+  onChange,
+}: {
+  defaultValues: Permissions[];
+  onChange: (newValues: Permissions[]) => void;
+}) => {
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+    onDropdownOpen: () => combobox.updateSelectedOptionIndex('active'),
+  });
+  const [search, setSearch] = useState('');
+
+  const [value, setValue] = useState<Permissions[]>([...defaultValues]);
+
+  useEffect(() => {
+    onChange(value);
+  }, [value]);
+
+  const handleValueSelect = (perm: Permissions) => {
+    setValue((current) =>
+      current.includes(perm) ? current.filter((v) => v !== perm) : [...current, perm],
+    );
+  };
+  const handleValueRemove = (val: Permissions) =>
+    setValue((current) => current.filter((v) => v !== val));
+
+  const values = value.map((item) => (
+    <Pill key={item} withRemoveButton onRemove={() => handleValueRemove(item)}>
+      {permissionsMap[item]}
+    </Pill>
+  ));
+
+  const options = Object.entries(permissionsMap)
+    .filter(([_, name]) => name.toLowerCase().includes(search.trim().toLowerCase()))
+    .map(([perm, name]) => (
+      <Combobox.Option value={name} key={perm} active={true}>
+        <Group gap='sm'>
+          {value.includes(Number(perm)) ? <CheckIcon size={12} /> : null}
+          <span>{name}</span>
+        </Group>
+      </Combobox.Option>
+    ));
+  return (
+    <>
+      <Combobox
+        store={combobox}
+        onOptionSubmit={(perm) => handleValueSelect(Number(getKeyByValue(permissionsMap, perm)))}
+      >
+        <Combobox.DropdownTarget>
+          <PillsInput onClick={() => combobox.openDropdown()} label='Permissions'>
+            <Pill.Group>
+              {values}
+
+              <Combobox.EventsTarget>
+                <PillsInput.Field
+                  onFocus={() => combobox.openDropdown()}
+                  onBlur={() => combobox.closeDropdown()}
+                  value={search}
+                  placeholder='Search values'
+                  onChange={(event) => {
+                    combobox.updateSelectedOptionIndex();
+                    setSearch(event.currentTarget.value);
+                  }}
+                />
+              </Combobox.EventsTarget>
+            </Pill.Group>
+          </PillsInput>
+        </Combobox.DropdownTarget>
+
+        <Combobox.Dropdown>
+          <Combobox.Options>
+            {options.length > 0 ? options : <Combobox.Empty>Nothing found...</Combobox.Empty>}
+          </Combobox.Options>
+        </Combobox.Dropdown>
+      </Combobox>
+    </>
+  );
+};
+
+export default PermissionsInput;
