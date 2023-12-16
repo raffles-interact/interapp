2f4984534b5c37fbf3095c112feb8b3710eec2af
SebassNoob <sebastian.ong@hotmail.com>
2023-12-16 19:10:23 +0800

    feat: admin page

---
+++ interapp-frontend/src/app/admin/AdminTable/DeleteAction/DeleteAction.tsx
@@ -0,0 +1,61 @@
+'use client';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, ActionIcon, Text, Button } from '@mantine/core';
+import { IconTrash } from '@tabler/icons-react';
+import { memo, useState } from 'react';
+import APIClient from '@/api/api_client';
+import './styles.css';
+
+function DeleteAction({ username, refreshData }: { username: string; refreshData: () => void }) {
+  const apiClient = new APIClient().instance;
+  const [opened, { open, close }] = useDisclosure(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleConfirm = async () => {
+    setLoading(true);
+
+    await apiClient.delete('/user', {
+      data: { username },
+    });
+
+    refreshData();
+    close();
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title='Delete User Data'
+        closeOnClickOutside={false}
+        closeOnEscape={false}
+        withCloseButton={false}
+      >
+        <div className='delete-modal'>
+          <Text>
+            Are you sure you want to delete this user:{' '}
+            <span className='delete-modal-username'>{username}</span>? All data related to the user
+            will be lost, including announcements, services, service sessions and more! This action
+            cannot be undone.
+          </Text>
+          <div className='delete-modal-buttons'>
+            <Button onClick={close} variant='outline' color='gray' disabled={loading}>
+              Cancel
+            </Button>
+            <Button onClick={handleConfirm} variant='outline' color='red' loading={loading}>
+              Delete
+            </Button>
+          </div>
+        </div>
+      </Modal>
+
+      <ActionIcon size={36} onClick={open} color='red' className='action-icon'>
+        <IconTrash />
+      </ActionIcon>
+    </>
+  );
+}
+
+export default memo(DeleteAction);
--- interapp-frontend/src/app/admin/page.tsx
+++ interapp-frontend/src/app/admin/page.tsx
@@ -1,14 +1,18 @@
-import { Table } from "@mantine/core";
-import APIClient from "@/api/api_client";
+import AdminTable from './AdminTable/AdminTable';
+import { Text, Title } from '@mantine/core';
+import './styles.css';
 
 export default async function AdminPage() {
-  const apiClient = new APIClient({useClient: false}).instance;
-
-  
-
   return (
-    <div>
-      <h1>Admin Page</h1>
+    <div className='admin-page'>
+      <div>
+        <Title order={1}>Admin Page</Title>
+        <Text>
+          This page allows for admins to modify and edit raw user data. Warning, changes may cause
+          security breaches if performed incorrectly.
+        </Text>
+      </div>
+      <AdminTable />
     </div>
   );
 }
