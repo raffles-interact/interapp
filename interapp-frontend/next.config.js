95db5f483d96ecd0100acbfc61948a7a030ba1dd
SebassNoob <sebastian.ong@hotmail.com>
2023-12-20 17:03:37 +0800

    chore: revert breaking changes, optimise further

--- interapp-frontend/next.config.js
+++ interapp-frontend/next.config.js
@@ -31,13 +31,16 @@ const nextConfig = {
     ];
   },
   experimental: {
-    optimizePackageImports: process.env.NODE_ENV === 'production' && [
-      '@mantine/core',
-      '@mantine/hooks',
-      '@mantine/form',
-      '@mantine/dates',
-      '@mantine/notifications',
-    ],
+    optimizePackageImports:
+      process.env.NODE_ENV === 'production'
+        ? [
+            '@mantine/core',
+            '@mantine/hooks',
+            '@mantine/form',
+            '@mantine/dates',
+            '@mantine/notifications',
+          ]
+        : null,
   },
 };
 
