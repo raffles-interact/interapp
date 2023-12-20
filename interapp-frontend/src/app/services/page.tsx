a7a368504e3e9c23fdbca3316c13dac9365f9f7d
SebassNoob <sebastian.ong@hotmail.com>
2023-12-20 16:32:20 +0800

    chore: clean up code smells

--- interapp-frontend/src/app/services/page.tsx
+++ interapp-frontend/src/app/services/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic'; // nextjs needs this to build properly
-
 import { Suspense, lazy } from 'react';
 import APIClient from '@/api/api_client';
 const ServiceBox = lazy(() => import('./ServiceBox/ServiceBox'));
