6936db616401d1d6d4cba917211cc280c3d4de10
SebassNoob <sebastian.ong@hotmail.com>
2024-01-01 17:24:34 +0800

    feat: add edit service

---
+++ interapp-frontend/src/app/services/EditService/EditService.tsx
@@ -0,0 +1,174 @@
+'use client';
+
+import CRUDModal from '@components/CRUDModal/CRUDModal';
+import APIClient from '@api/api_client';
+import { Service } from '../types';
+import { useDisclosure } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
+import { TextInput, Textarea, NumberInput, Button, Group } from '@mantine/core';
+import SearchableSelect from '@components/SearchableSelect/SearchableSelect';
+import { daysOfWeek } from '../ServiceBox/ServiceBox';
+import { type Nullable } from '@api/utils';
+import { TimeInput } from '@mantine/dates';
+import { useState, useContext, memo, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import UploadImage, { convertToBase64, allowedFormats } from '@components/UploadImage/UploadImage';
+import { IconPencil } from '@tabler/icons-react';
+import { Permissions } from '@/app/route_permissions';
+import { AuthContext } from '@providers/AuthProvider/AuthProvider';
+import './styles.css';
+import { notifications } from '@mantine/notifications';
+
+const parsePromotionalImage = async (promotional_image: string | null | undefined) => {
+  if (!promotional_image) return '';
+  if (promotional_image.startsWith('http'))
+    return await convertToBase64(new URL(promotional_image));
+  return promotional_image;
+};
+
+const EditService = ({
+  service_id,
+  name,
+  description,
+  promotional_image,
+  contact_email,
+  contact_number,
+  website,
+  start_time,
+  end_time,
+  day_of_week,
+}: Omit<Service, 'service_ic_username'>) => {
+  const apiClient = new APIClient().instance;
+  const { user } = useContext(AuthContext);
+  const [opened, { open, close }] = useDisclosure();
+  const [loading, setLoading] = useState(false);
+
+  const [promotionalImgBase64, setPromotionalImgBase64] = useState<string>();
+  useEffect(() => {
+    if (!opened) return;
+    parsePromotionalImage(promotional_image).then((base64) => {
+      setPromotionalImgBase64(base64);
+      form.setFieldValue('promotional_image', base64);
+    });
+  }, [opened]);
+
+  const router = useRouter();
+
+  const initial = {
+    name,
+    description: description ?? '',
+    promotional_image: promotionalImgBase64 ?? '',
+    contact_email,
+    contact_number: contact_number ?? undefined,
+    website: website ?? '',
+    start_time,
+    end_time,
+    day_of_week,
+  };
+
+  const form = useForm({
+    initialValues: initial,
+    validate: {
+      name: (value) => value.trim().length <= 2 && 'Name must be longer than 2 characters',
+      contact_email: (value) => !value.includes('@') && 'Invalid email',
+      contact_number: (value) => {
+        if (!value) return false;
+        return value.toString().length !== 8 && 'Invalid phone number';
+      },
+      website: (value) => {
+        if (!value) return false;
+        return !value.includes('http') && 'Invalid website';
+      },
+      end_time: (value, values) =>
+        value <= values.start_time && 'End time must be after start time',
+    },
+  });
+
+  const handleSubmit = async (data: Omit<Service, 'service_ic_username' | 'service_id'>) => {
+    setLoading(true);
+    const body: Nullable<Partial<typeof initial>> & { service_id: number } = {
+      ...data,
+      service_id,
+      description: data.description ? data.description : null,
+      contact_number: data.contact_number ? data.contact_number : null,
+      website: data.website ? data.website : null,
+      promotional_image: data.promotional_image ? data.promotional_image : null,
+    };
+
+    const res = await apiClient.patch(`/service`, body);
+    if (res.status !== 200) {
+      notifications.show({
+        title: 'Error',
+        message: res.data,
+        color: 'red',
+      });
+      setLoading(false);
+      return;
+    }
+    notifications.show({
+      title: 'Success',
+      message: 'Service updated successfully',
+      color: 'green',
+    });
+
+    setLoading(false);
+    router.refresh();
+    close();
+  };
+
+  return (
+    <CRUDModal
+      opened={opened}
+      open={open}
+      close={close}
+      title='Add Service'
+      Icon={IconPencil}
+      show={() => !!user && user.permissions.includes(Permissions.EXCO)}
+    >
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <div className='edit-service'>
+          <UploadImage
+            onChange={async (_, file) =>
+              form.setFieldValue('promotional_image', await convertToBase64(file))
+            }
+            accept={allowedFormats}
+            className='edit-service-file-display'
+            defaultImageURL={promotional_image}
+          />
+          <TextInput label='Service Name' required {...form.getInputProps('name')} />
+          <Textarea label='Description' {...form.getInputProps('description')} />
+          <TextInput label='Contact Email' required {...form.getInputProps('contact_email')} />
+          <NumberInput label='Contact Number' {...form.getInputProps('contact_number')} />
+          <TextInput label='Website' {...form.getInputProps('website')} />
+          <SearchableSelect
+            defaultValue={'Sun'}
+            allValues={daysOfWeek}
+            onChange={(day_of_week) =>
+              form.setFieldValue(
+                'day_of_week',
+                daysOfWeek.indexOf(day_of_week) as 0 | 1 | 2 | 3 | 4 | 5 | 6,
+              )
+            }
+            label='Day of Week'
+            required
+          />
+          <div className='edit-service-times'>
+            <TimeInput label='Start Time' required {...form.getInputProps('start_time')} />
+            <TimeInput label='End Time' required {...form.getInputProps('end_time')} />
+          </div>
+
+          <Group gap={3} justify='center'>
+            <Button onClick={close} variant='outline' color='red'>
+              Cancel
+            </Button>
+            <Button type='submit' variant='outline' color='green' loading={loading}>
+              Submit
+            </Button>
+          </Group>
+        </div>
+      </form>
+    </CRUDModal>
+  );
+};
+
+export default memo(EditService);
