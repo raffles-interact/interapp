a8396a20e75ca8aaffa32c1681bdc516e759578b
SebassNoob <sebastian.ong@hotmail.com>
2024-01-02 21:36:46 +0800

    feat: add QR functionality

--- interapp-frontend/src/app/attendance/page.tsx
+++ interapp-frontend/src/app/attendance/page.tsx
@@ -1,28 +1,15 @@
-import APIClient from '@api/api_client';
 import AttendanceMenu from './AttendanceMenu/AttendanceMenu';
-
-export const fetchActiveServiceSessions = async () => {
-  const apiClient = new APIClient().instance;
-  const response = await apiClient.get('/service/active_sessions');
-  if (response.status !== 200) throw new Error('Failed to fetch active service sessions');
-
-  const data: {
-    [hash: string]: {
-      service_session_id: number;
-      ICs: string[];
-    };
-  }[] = response.data;
-  return data;
-};
-
-export type fetchActiveServiceSessionsType = Awaited<ReturnType<typeof fetchActiveServiceSessions>>;
 
 export default async function AttendancePage({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  if (searchParams['id'] === undefined) {
-    return <AttendanceMenu />;
-  }
+  // if there is an id, it must be a number
+  if (
+    searchParams.id instanceof Array ||
+    (searchParams.id && !/^\d+$/.test(searchParams.id as string))
+  )
+    throw new Error('Invalid id');
+  return <AttendanceMenu id={searchParams.id ? parseInt(searchParams.id as string) : undefined} />;
 }
