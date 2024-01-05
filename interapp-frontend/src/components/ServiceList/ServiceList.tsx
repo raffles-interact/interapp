1ca270a76a9b3b2a3bf1ef5afd66d4612860e020
TRAINEDTURTLE <cjxz2006@gmail.com>
2024-01-06 00:45:06 +0800

    feat: template for homepage with example content, API not implemented yet

---
+++ interapp-frontend/src/components/ServiceList/ServiceList.tsx
@@ -0,0 +1,14 @@
+import { Text, Title } from '@mantine/core';
+import './styles.css';
+
+export default function ServiceList(props: any) {
+  const temp_services = ['Service 1: Information', 'Service 2: A lot of information']
+  return (
+    <div className='service-list-container'>
+      <Title>Service(s):</Title>
+      {temp_services.map((service) => (
+        <Text key={service} className='service'>{service}</Text>
+      ))}
+    </div>
+  );
+}
