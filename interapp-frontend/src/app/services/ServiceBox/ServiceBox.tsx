e547355861659e5fb1ff868af4776385d60e4b62
SebassNoob <sebastian.ong@hotmail.com>
2024-01-05 12:53:36 +0800

    chore: service box styling change

--- interapp-frontend/src/app/services/ServiceBox/ServiceBox.tsx
+++ interapp-frontend/src/app/services/ServiceBox/ServiceBox.tsx
@@ -127,7 +127,7 @@ const ServiceBox = (service: Service & { alreadyServiceICUsernames: string[] }) 
           <Title order={3} className='service-box-info-title'>
             {service.name}
           </Title>
-          <Text>{service.description ?? 'No description provided :('}</Text>
+          <Text size='sm' c='dimmed'>{service.description ?? 'No description provided :('}</Text>
         </div>
 
         <div className='service-box-info-content'>
