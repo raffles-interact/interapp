ee0cf221de4bca11805b81fad4f2e61577e279c8
SebassNoob <sebastian.ong@hotmail.com>
2024-01-14 14:25:13 +0800

    feat: add tiptap and dropzone deps

--- interapp-frontend/src/providers/MantineAppProvider/MantineAppProvider.tsx
+++ interapp-frontend/src/providers/MantineAppProvider/MantineAppProvider.tsx
@@ -1,6 +1,8 @@
 import '@mantine/core/styles.css'; // import core styles first
 import '@mantine/notifications/styles.css';
 import '@mantine/dates/styles.css';
+import '@mantine/tiptap/styles.css';
+import '@mantine/dropzone/styles.css';
 
 import { ReactNode } from 'react';
 import { DatesProvider } from '@mantine/dates';
