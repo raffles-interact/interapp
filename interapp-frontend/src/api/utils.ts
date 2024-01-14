644e8f63f012684e6e6de98f7662f064dbd2e3c2
SebassNoob <sebastian.ong@hotmail.com>
2024-01-14 14:25:46 +0800

    feat: allow wildcard parsing of routes

--- interapp-frontend/src/api/utils.ts
+++ interapp-frontend/src/api/utils.ts
@@ -16,6 +16,23 @@ export async function getAllUsernames() {
   return allUsersNames;
 }
 
+export function wildcardMatcher(str: string, pattern: string) {
+  // Add a trailing slash to the pattern if it doesn't have one
+  if (!pattern.endsWith('/')) {
+    pattern += '/';
+  }
+  // Add a trailing slash to the string if it doesn't have one
+  if (!str.endsWith('/')) {
+    str += '/';
+  }
+  // Replace '*' with '.*' to create a regex pattern
+  const regexPattern = `^${pattern.replace(/\*/g, '.*')}$`;
+  // Create a new RegExp object
+  const regex = new RegExp(regexPattern);
+  // Test the url against the regex pattern
+  return regex.test(str);
+}
+
 export type Nullable<T> = {
   [P in keyof T]: T[P] | null;
 };
