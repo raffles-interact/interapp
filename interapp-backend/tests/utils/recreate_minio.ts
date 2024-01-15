a95daff59940d6daabe0d748822b81feaae2cbf8
SebassNoob <sebastian.ong@hotmail.com>
2024-01-15 19:30:04 +0800

    fix: fix remaining api tests

--- interapp-backend/tests/utils/recreate_minio.ts
+++ interapp-backend/tests/utils/recreate_minio.ts
@@ -2,7 +2,11 @@ import minioClient, { createBucket } from '@utils/init_minio';
 
 export const recreateMinio = async () => {
   try {
-    const stream = await minioClient.listObjects(process.env.MINIO_BUCKETNAME as string);
+    const stream = await minioClient.listObjectsV2(
+      process.env.MINIO_BUCKETNAME as string,
+      undefined,
+      true,
+    );
     stream.on('data', async (obj) => {
       if (obj.name)
         await minioClient.removeObject(process.env.MINIO_BUCKETNAME as string, obj.name);
