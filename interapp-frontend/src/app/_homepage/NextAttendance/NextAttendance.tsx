cf7c26a9e014a17742d062210ca17c47fe6eb98d
SebassNoob <sebastian.ong@hotmail.com>
2024-01-20 17:37:35 +0800

    feat: add next session

---
+++ interapp-frontend/src/app/_homepage/NextAttendance/NextAttendance.tsx
@@ -0,0 +1,27 @@
+import { Text, Title, Paper } from '@mantine/core';
+import { type FetchAttendanceResponse } from '../AttendanceList/AttendanceList';
+import './styles.css';
+
+interface NextAttendanceProps {
+  nextSession: FetchAttendanceResponse[0] | null;
+}
+
+export default function NextAttendance({ nextSession }: NextAttendanceProps) {
+  if (nextSession === null) {
+    return (
+      <Paper shadow='sm' p='md' radius='md'>
+        <Text c='dimmed'>No upcoming services</Text>
+      </Paper>
+    );
+  }
+  return (
+    <Paper shadow='sm' p='md' radius='md'>
+      <Title order={4}>{nextSession.name}</Title>
+      <Text c='dimmed'>
+        {new Date(nextSession.start_time).toLocaleString('en-GB')} -{' '}
+        {new Date(nextSession.end_time).toLocaleString('en-GB')}{' '}(
+        {new Date(nextSession.start_time).toLocaleString('en-GB', { weekday: 'long' })})
+      </Text>
+    </Paper>
+  );
+}
