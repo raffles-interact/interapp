b8336751569cf58888425f1cd083ece5c3988bb0
SebassNoob <sebastian.ong@hotmail.com>
2024-01-24 18:13:41 +0800

    feat: add ratelimit handling

--- interapp-frontend/src/api/api_client.ts
+++ interapp-frontend/src/api/api_client.ts
@@ -8,16 +8,21 @@ export interface APIClientConfig {
 export class APIClient {
   public readonly instance: AxiosInstance;
   private readonly config: APIClientConfig;
+  private abortController: AbortController = new AbortController();
   public constructor(config?: APIClientConfig) {
     const defaults: APIClientConfig = {
       useClient: true,
       useMultiPart: false,
     };
     this.config = { ...defaults, ...config };
+
     this.instance = axios.create({
       timeout: 60000,
       withCredentials: true,
-      validateStatus: (status) => status < 500,
+      signal: this.abortController.signal,
+      validateStatus: (status) => {
+        return status !== 429 && status < 500;
+      },
       maxContentLength: Infinity,
       maxBodyLength: Infinity,
       proxy: false,
@@ -26,6 +31,9 @@ export class APIClient {
         : `http://${process.env.NEXT_PUBLIC_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api`,
     });
     this.instance.interceptors.request.use((req) => {
+      if (window.location.pathname === '/error/429') {
+        this.abortController.abort('Too many requests');
+      }
       if (this.config.useMultiPart) req.headers['Content-Type'] = 'multipart/form-data';
       else req.headers['Content-Type'] = 'application/json';
 
@@ -40,10 +48,14 @@ export class APIClient {
     this.instance.interceptors.response.use(
       (res) => res,
       (err) => {
-        const statusCode: number = err.response.status;
+        const statusCode: number = err?.response?.status;
+        const headers = err?.response?.headers;
+
         if (statusCode === 429) {
-          window.location.href = '/error/429';
+          this.abortController.abort('Too many requests');
+          window.location.href = `/error/429?reset=${headers['ratelimit-reset']}&policy=${headers['ratelimit-policy']}`;
         }
+
         throw err;
       },
     );
