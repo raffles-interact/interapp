0e476358d78fff51a617a38b24c81b89e4098a00
SebassNoob <sebastian.ong@hotmail.com>
2024-03-08 14:37:45 +0800

    chore: prettier

--- interapp-backend/api/utils/errors.ts
+++ interapp-backend/api/utils/errors.ts
@@ -59,23 +59,96 @@ export const TestErrors = {
 };
 
 export const HTTPErrors = {
-  INVALID_DATA_URL: new HTTPError('InvalidDataUrl', 'Image is not a valid data URL', HTTPErrorCode.BAD_REQUEST_ERROR),
-  ALREADY_EXISTS: new HTTPError('AlreadyExists', 'Resource already exists', HTTPErrorCode.CONFLICT_ERROR),
-  RESOURCE_NOT_FOUND: new HTTPError('ResourceNotFound', 'Resource not found', HTTPErrorCode.NOT_FOUND_ERROR),
-  FAILED_HASHING_PASSWORD: new HTTPError('FailedHashingPassword', 'An error occurred while hashing the password', HTTPErrorCode.INTERNAL_SERVER_ERROR),
-  INVALID_PASSWORD: new HTTPError('InvalidPassword', 'The password you entered is incorrect', HTTPErrorCode.UNAUTHORIZED_ERROR),
-  INVALID_REFRESH_TOKEN: new HTTPError('InvalidRefreshToken', 'The refresh token is invalid', HTTPErrorCode.UNAUTHORIZED_ERROR),
-  INVALID_ACCESS_TOKEN: new HTTPError('InvalidAccessToken', 'The access token is invalid', HTTPErrorCode.UNAUTHORIZED_ERROR),
-  INVALID_HASH: new HTTPError('InvalidHash', 'The hash is invalid', HTTPErrorCode.BAD_REQUEST_ERROR),
-  INVALID_TIME_INTERVAL: new HTTPError('InvalidTimeInterval', 'The time interval given is not possible', HTTPErrorCode.BAD_REQUEST_ERROR),
-  AD_HOC_NOT_ENABLED: new HTTPError('AdHocNotEnabled', 'Ad hoc mode is not enabled', HTTPErrorCode.FORBIDDEN_ERROR),
-  ALREADY_ATTENDED: new HTTPError('AlreadyAttended', 'You have already attended this event', HTTPErrorCode.CONFLICT_ERROR),
-  ALREADY_VERIFIED: new HTTPError('AlreadyVerified', 'You have already verified your email', HTTPErrorCode.CONFLICT_ERROR),
-  ALREADY_PART_OF_SERVICE: new HTTPError('AlreadyPartOfService', 'You are already part of this service', HTTPErrorCode.CONFLICT_ERROR),
-  NO_SERVICES_FOUND: new HTTPError('NoServicesFound', 'This user is not part of any service', HTTPErrorCode.NOT_FOUND_ERROR),
-  NO_SERVICE_SESSION_FOUND: new HTTPError('NoServiceSessionFound', 'This user is not part of any service session', HTTPErrorCode.NOT_FOUND_ERROR),
-  SERVICE_NO_USER_FOUND: new HTTPError('NoUserFound', 'Service has no users', HTTPErrorCode.NOT_FOUND_ERROR),
-  DATABASE_CORRUPTED: new HTTPError('DatabaseCorrupted', 'The database contains invalid data or is corrupted', HTTPErrorCode.INTERNAL_SERVER_ERROR),
-  INSUFFICIENT_PERMISSIONS: new HTTPError('InsufficientPermissions', 'You do not have sufficient permissions to perform this action', HTTPErrorCode.FORBIDDEN_ERROR),
-  GENERALISED_DB_ERROR: (message: unknown) => new HTTPError('GeneralisedDBError', String(message), HTTPErrorCode.INTERNAL_SERVER_ERROR),
-}
+  INVALID_DATA_URL: new HTTPError(
+    'InvalidDataUrl',
+    'Image is not a valid data URL',
+    HTTPErrorCode.BAD_REQUEST_ERROR,
+  ),
+  ALREADY_EXISTS: new HTTPError(
+    'AlreadyExists',
+    'Resource already exists',
+    HTTPErrorCode.CONFLICT_ERROR,
+  ),
+  RESOURCE_NOT_FOUND: new HTTPError(
+    'ResourceNotFound',
+    'Resource not found',
+    HTTPErrorCode.NOT_FOUND_ERROR,
+  ),
+  FAILED_HASHING_PASSWORD: new HTTPError(
+    'FailedHashingPassword',
+    'An error occurred while hashing the password',
+    HTTPErrorCode.INTERNAL_SERVER_ERROR,
+  ),
+  INVALID_PASSWORD: new HTTPError(
+    'InvalidPassword',
+    'The password you entered is incorrect',
+    HTTPErrorCode.UNAUTHORIZED_ERROR,
+  ),
+  INVALID_REFRESH_TOKEN: new HTTPError(
+    'InvalidRefreshToken',
+    'The refresh token is invalid',
+    HTTPErrorCode.UNAUTHORIZED_ERROR,
+  ),
+  INVALID_ACCESS_TOKEN: new HTTPError(
+    'InvalidAccessToken',
+    'The access token is invalid',
+    HTTPErrorCode.UNAUTHORIZED_ERROR,
+  ),
+  INVALID_HASH: new HTTPError(
+    'InvalidHash',
+    'The hash is invalid',
+    HTTPErrorCode.BAD_REQUEST_ERROR,
+  ),
+  INVALID_TIME_INTERVAL: new HTTPError(
+    'InvalidTimeInterval',
+    'The time interval given is not possible',
+    HTTPErrorCode.BAD_REQUEST_ERROR,
+  ),
+  AD_HOC_NOT_ENABLED: new HTTPError(
+    'AdHocNotEnabled',
+    'Ad hoc mode is not enabled',
+    HTTPErrorCode.FORBIDDEN_ERROR,
+  ),
+  ALREADY_ATTENDED: new HTTPError(
+    'AlreadyAttended',
+    'You have already attended this event',
+    HTTPErrorCode.CONFLICT_ERROR,
+  ),
+  ALREADY_VERIFIED: new HTTPError(
+    'AlreadyVerified',
+    'You have already verified your email',
+    HTTPErrorCode.CONFLICT_ERROR,
+  ),
+  ALREADY_PART_OF_SERVICE: new HTTPError(
+    'AlreadyPartOfService',
+    'You are already part of this service',
+    HTTPErrorCode.CONFLICT_ERROR,
+  ),
+  NO_SERVICES_FOUND: new HTTPError(
+    'NoServicesFound',
+    'This user is not part of any service',
+    HTTPErrorCode.NOT_FOUND_ERROR,
+  ),
+  NO_SERVICE_SESSION_FOUND: new HTTPError(
+    'NoServiceSessionFound',
+    'This user is not part of any service session',
+    HTTPErrorCode.NOT_FOUND_ERROR,
+  ),
+  SERVICE_NO_USER_FOUND: new HTTPError(
+    'NoUserFound',
+    'Service has no users',
+    HTTPErrorCode.NOT_FOUND_ERROR,
+  ),
+  DATABASE_CORRUPTED: new HTTPError(
+    'DatabaseCorrupted',
+    'The database contains invalid data or is corrupted',
+    HTTPErrorCode.INTERNAL_SERVER_ERROR,
+  ),
+  INSUFFICIENT_PERMISSIONS: new HTTPError(
+    'InsufficientPermissions',
+    'You do not have sufficient permissions to perform this action',
+    HTTPErrorCode.FORBIDDEN_ERROR,
+  ),
+  GENERALISED_DB_ERROR: (message: unknown) =>
+    new HTTPError('GeneralisedDBError', String(message), HTTPErrorCode.INTERNAL_SERVER_ERROR),
+};
