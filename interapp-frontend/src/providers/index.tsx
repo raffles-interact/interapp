853f1403c75ababca068cff9719c6d32affa4b86
SebassNoob <sebastian.ong@hotmail.com>
2024-04-07 00:41:31 +0800

    fix: prerender error on useSearchParams

--- interapp-frontend/src/providers/index.tsx
+++ interapp-frontend/src/providers/index.tsx
@@ -1,12 +1,14 @@
 'use client';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import { AuthProvider } from '@providers/AuthProvider/AuthProvider';
 import { MantineAppProvider } from './MantineAppProvider/MantineAppProvider';
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
     <MantineAppProvider>
-      <AuthProvider>{children}</AuthProvider>
+      <Suspense>
+        <AuthProvider>{children}</AuthProvider>
+      </Suspense>
     </MantineAppProvider>
   );
 };
