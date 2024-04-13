1d970d170ef68086eb3843a8ca7d6be89dec6c8b
SebassNoob <sebastian.ong@hotmail.com>
2024-04-14 02:53:45 +0800

    chore: add release for sentry observability

--- interapp-frontend/sentry.client.config.js
+++ interapp-frontend/sentry.client.config.js
@@ -4,6 +4,7 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 Sentry.init({
   enabled: process.env.NODE_ENV === 'production',
+  release: process.env.NEXT_PUBLIC_VERSION ? process.env.NEXT_PUBLIC_VERSION : undefined,
   // dsn can be public, no security risk here
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   // Replay may only be enabled for the client-side
