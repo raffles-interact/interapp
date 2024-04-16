68cc7b2204963064f871005ccf1fb6d334e14172
SebassNoob <sebastian.ong@hotmail.com>
2024-04-16 17:31:27 +0800

    chore: remove newlines

--- interapp-backend/tests/unit/ExportsModel.test.ts
+++ interapp-backend/tests/unit/ExportsModel.test.ts
@@ -3,12 +3,9 @@ import { ExportsModel } from '@models/.';
 import { User } from '@db/entities';
 import { describe, test, expect } from 'bun:test';
 
-
 const SUITE_NAME = 'ExportsModel';
 const suite = testSuites[SUITE_NAME];
 
 console.log(suite);
 
-
 runSuite(SUITE_NAME, suite);
-
