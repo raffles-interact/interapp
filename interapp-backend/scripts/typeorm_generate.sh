0c6353f997aa591e31ff7ffd1887a0f964847a90
SebassNoob <sebastian.ong@hotmail.com>
2024-04-17 20:54:46 +0800

    fix: incorrect pathing for renamed data_source

--- interapp-backend/scripts/typeorm_generate.sh
+++ interapp-backend/scripts/typeorm_generate.sh
@@ -7,5 +7,5 @@ then
   echo "Please provide a name for the migration"
   exit 1
 fi
-bun run typeorm migration:generate db/migrations/$1 -d db/data-source.ts 
+bun run typeorm migration:generate db/migrations/$1 -d db/data_source.ts 
 
