4ced19345027ddf6122267d27cb11cd4b2cad839
SebassNoob <sebastian.ong@hotmail.com>
2024-04-18 22:34:30 +0800

    fix: restrict endpoint

--- interapp-backend/api/routes/endpoints/exports/exports.ts
+++ interapp-backend/api/routes/endpoints/exports/exports.ts
@@ -1,14 +1,15 @@
 import { ExportsModel } from '@models/exports';
 import { z } from 'zod';
-import { validateRequiredFieldsV2 } from '@routes/middleware';
+import { validateRequiredFieldsV2, verifyJWT, verifyRequiredPermission } from '@routes/middleware';
 import { ExportsFields } from './validation';
 import { Router } from 'express';
+import { Permissions } from '@utils/permissions';
 
 export const exportsRouter = Router();
 
 const xlsxMime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
 
-exportsRouter.get('/', validateRequiredFieldsV2(ExportsFields), async (req, res) => {
+exportsRouter.get('/', validateRequiredFieldsV2(ExportsFields), verifyJWT, verifyRequiredPermission(Permissions.ATTENDANCE_MANAGER), async (req, res) => {
   const query = req.query as unknown as z.infer<typeof ExportsFields>;
 
   const exports = await ExportsModel.packXLSX(query.id, query.start_date, query.end_date);
