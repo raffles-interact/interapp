bd3e9b674b72a8c8df0618a92d24474d85e4cc82
SebassNoob <sebastian.ong@hotmail.com>
2024-04-22 20:40:18 +0800

    fix: final fix?

--- interapp-backend/eslint.config.js
+++ interapp-backend/eslint.config.js
@@ -1,7 +1,6 @@
 import 'eslint-plugin-only-warn';
-import * as eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(...tseslint.configs.recommended, {
-  ignores: ['node_modules/', 'pgdata/', 'minio-data/', 'tests/', 'scripts/'],
+  ignores: ['node_modules/', 'pgdata/', 'minio-data/'],
 });
