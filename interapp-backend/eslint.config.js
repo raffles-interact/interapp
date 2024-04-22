d1264f5da8eb9b0e24847ba76c2cc8d1402f8b87
SebassNoob <sebastian.ong@hotmail.com>
2024-04-22 19:53:34 +0800

    fix: combine tests, add warning lint

--- interapp-backend/eslint.config.js
+++ interapp-backend/eslint.config.js
@@ -2,11 +2,8 @@
 
 import * as eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import 'eslint-plugin-only-warn';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    ignores: ['node_modules/', 'pgdata/', 'minio-data/', 'tests/','scripts/'],
-  }
-);
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+  ignores: ['node_modules/', 'pgdata/', 'minio-data/', 'tests/', 'scripts/'],
+});
