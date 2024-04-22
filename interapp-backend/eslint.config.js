043b44f8370873b99882dc7c0992121264b9aa78
SebassNoob <sebastian.ong@hotmail.com>
2024-04-22 20:13:11 +0800

    fix: attempt fix of eslint ci?

--- interapp-backend/eslint.config.js
+++ interapp-backend/eslint.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 
+import 'eslint-plugin-only-warn';
 import * as eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import 'eslint-plugin-only-warn';
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
   ignores: ['node_modules/', 'pgdata/', 'minio-data/', 'tests/', 'scripts/'],
