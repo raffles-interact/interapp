abbe26cb32a86947c81e7eb6c06976c538092f2a
SebassNoob <sebastian.ong@hotmail.com>
2024-04-22 19:22:20 +0800

    feat: set up eslint

---
+++ interapp-backend/eslint.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+import * as eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['node_modules/', 'pgdata/', 'minio-data/', 'tests/','scripts/'],
+  }
+);
