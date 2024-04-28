8e9226e875c04b58ebe6e8a665398336b9bb52b9
SebassNoob <sebastian.ong@hotmail.com>
2024-04-28 20:02:40 +0800

    fix: weird failed condition for profile page

--- interapp-frontend/src/app/profile/page.tsx
+++ interapp-frontend/src/app/profile/page.tsx
@@ -6,18 +6,35 @@ import { ActiveTabContext } from './utils';
 import Overview from './Overview/Overview';
 import ServiceSessionsPage from './ServiceSessionsPage/ServiceSessionsPage';
 import ServiceCardDisplay from './ServiceCardDisplay/ServiceCardDisplay';
+import PageSkeleton from '@components/PageSkeleton/PageSkeleton';
+import { Text } from '@mantine/core';
+import GoHomeButton from '@/components/GoHomeButton/GoHomeButton';
+import './styles.css';
 
 export default function Profile() {
   const activeTab = useContext(ActiveTabContext);
 
-  const { user, updateUser } = useContext(AuthContext);
+  const { user, updateUser, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <PageSkeleton />;
+  }
+
+  if (!user) {
+    return (
+      <div className='profile-notfound-container'>
+        <Text>User not found. Please log in again.</Text>
+        <GoHomeButton />
+      </div>
+    );
+  }
 
   switch (activeTab) {
     case 'Overview':
-      return <Overview username={user?.username ?? ''} updateUser={updateUser} />;
+      return <Overview username={user.username} updateUser={updateUser} />;
     case 'Services':
-      return <ServiceCardDisplay username={user?.username ?? ''} />;
+      return <ServiceCardDisplay username={user.username} />;
     case 'Service Sessions':
-      return <ServiceSessionsPage username={user?.username ?? ''} />;
+      return <ServiceSessionsPage username={user.username} />;
   }
 }
