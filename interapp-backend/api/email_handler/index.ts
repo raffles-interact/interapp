7c2cc417e8207b05d564ab2de66579d5819911ad
AJR07 <angjunray@live.com>
2024-08-21 22:50:42 +0800

    :bug: Fix use of let export in email handler

--- interapp-backend/api/email_handler/index.ts
+++ interapp-backend/api/email_handler/index.ts
@@ -1,22 +1,23 @@
 import { Transporter, createTransport } from 'nodemailer';
 import hbs from 'nodemailer-express-handlebars';
 
-let transporter: Transporter;
-try {
-  transporter = createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER as string,
-      pass: process.env.EMAIL_PASSWORD as string,
-    },
-  });
-} catch (err) {
-  console.error(err);
-  process.exit(1);
-}
+const transporter: Transporter = (() => {
+  try {
+    return createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER as string,
+        pass: process.env.EMAIL_PASSWORD as string,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
 
 const handlebarsOptions: hbs.NodemailerExpressHandlebarsOptions = {
   viewEngine: {
@@ -30,16 +31,3 @@ const handlebarsOptions: hbs.NodemailerExpressHandlebarsOptions = {
 transporter.use('compile', hbs(handlebarsOptions));
 
 export default transporter;
-
-/*
-await transporter.sendMail({
-  from: {
-    name: 'hi',
-    address: process.env.EMAIL_USER as string,
-  }, // sender address
-  to: ['sebastian.ong@hotmail.com'], // list of receivers
-  subject: "Hello ✔", // Subject line
-  text: "Hello world?", // plain text body
-  html: "<b>Hello world?</b>", // html body
-});
-*/
